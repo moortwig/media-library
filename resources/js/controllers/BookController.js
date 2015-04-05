f50d08c0f469abfa3050ef372549e539989a7fd4
moortwig <info@moortwig.se>
2015-04-05 16:22:09 +0200

    Rearranged data presentation

--- resources/js/controllers/BookController.js
+++ resources/js/controllers/BookController.js
@@ -2,7 +2,6 @@ var books = new Array(); // maybe remove this line
 
 console.log("BookController");
 mediaLibrary.controller('BookController', function($scope){
-	$scope.hello = "Hello world";
 	$scope.books = [{
 		"title": "Thief Of Time",
 		"author": "Terry Pratchett",
